abac0feb2ee4242743fd91c8d702dd6301a878d4
ankit <ankit.sanghavi87@gmail.com>
2022-02-26 15:53:08 +0530

    debug api res

--- pkg/handlers/api_response.go
+++ pkg/handlers/api_response.go
@@ -1,15 +1,16 @@
 package handlers
 
-import(
+import (
 	"encoding/json"
+
 	"github.com/aws/aws-lambda-go/events"
 )
 
-func apiResponse(status int, body interface{})(*events.APIGatewayProxyResponse, error){
-	resp := events.APIGatewayProxyResponse{Headers: map[string]string["Content-Type": "application/json"]}
+func apiResponse(status int, body interface{}) (*events.APIGatewayProxyResponse, error) {
+	resp := events.APIGatewayProxyResponse{Headers: map[string]string{"Content-Type": "application/json"}}
 	resp.StatusCode = status
-	
-	stringBody,_ := json.Marshal(body)
+
+	stringBody, _ := json.Marshal(body)
 	resp.Body = string(stringBody)
 	return &resp, nil
 }
